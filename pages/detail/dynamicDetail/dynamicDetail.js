09d26a6e4cb06a72a1ce7b167b06688590433ffe
Alix00544 <439601575@qq.com>
2020-04-13 01:48:35 +0800

    动态详情页面 UI完成
    动态详情页面 UI完成

--- pages/detail/dynamicDetail/dynamicDetail.js
+++ pages/detail/dynamicDetail/dynamicDetail.js
@@ -1,66 +1,104 @@
-// pages/detail/dynamicDetail/dynamicDetail.js
+const app = getApp();
+const util = require('../../../utils/util');
+const cloudCallBase = app.globalData.cloudCallBase;
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        StatusBar: app.globalData.StatusBar,
+        CustomBar: app.globalData.CustomBar,
+        WinHeight: app.globalData.WinHeight,
+        ScreenWidth: app.globalData.ScreenWidth,
+        Custom: app.globalData.Custom,
+        navBarOpacity: 0,
+        navBarFrontColor: "#ffffff",
+        bgImg: "https://s1.ax1x.com/2020/04/05/Gr0HN4.jpg",
+        swiperList: ["https://s1.ax1x.com/2020/04/05/Gr0HN4.jpg", "https://s1.ax1x.com/2020/04/05/Gr0HN4.jpg", "https://s1.ax1x.com/2020/04/05/Gr0HN4.jpg"],
+        scrollViewEnableHeight: app.globalData.ScreenWidth - app.globalData.CustomBar
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(options) {
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function() {
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function() {
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+    },
+    onPageScroll: function(e) {
+        // 实现吸顶效果
+        var scrollTop = e.scrollTop;
+        // 实现顶部栏渐变效果，设置透明度变化
+        this.setOpacity(scrollTop, this.data.scrollViewEnableHeight - 10);
+    },
+    // 顶部栏opacity变化函数
+    setOpacity: function(scrollTop, maxTop) {
+        var opacity = 0;
+        var navBarFrontColor;
+        if (scrollTop <= maxTop) {
+            opacity = scrollTop / maxTop;
+            navBarFrontColor = '#ffffff';
+        } else {
+            opacity = 1;
+            navBarFrontColor = '#000000';
+        }
+        if (navBarFrontColor != this.data.navBarFrontColor) {
+            // 踩坑，不能单独设置frontColor，需要加上backgroundColor
+            wx.setNavigationBarColor({
+                frontColor: navBarFrontColor,
+                backgroundColor: navBarFrontColor
+            })
+        }
+        if (this.data.navBarOpacity != opacity) {
+            this.setData({
+                navBarOpacity: opacity,
+                navBarFrontColor: navBarFrontColor
+            })
+        }
+    },
+    BackPage: function() {
+        wx.navigateBack({
+            delta: 1
+        });
+    },
+    toHome: function() {
+        wx.reLaunch({
+            url: '/pages/tabbar/sing/sing',
+        })
+    },
 
-  },
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function() {
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
+    },
 
-  },
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function() {
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
+    },
 
-  },
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function() {
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+    }
 })
--- pages/tabbar/dynamic/dynamic.js
+++ pages/tabbar/dynamic/dynamic.js
@@ -1,155 +1,154 @@
-
 const app = getApp();
 const util = require('../../../utils/util')
 const cloudCallBase = app.globalData.cloudCallBase;
 Page({
-  data: {
-    StatusBar: app.globalData.StatusBar,  //状态栏高度
-    CustomBar: app.globalData.CustomBar,  //NavBar整体高度
-    WinHeight: app.globalData.WinHeight,  //可使用窗口高度
-    currentTab: 0,
-    // 动态内容
-    playingSongId: -1,
-    // 是否展示底部弹出评论栏
-    showComments: false,
-    // 当前动态的评论
-    curentComments: [],
-    hotDynamicList: []
-  },
+    data: {
+        StatusBar: app.globalData.StatusBar, //状态栏高度
+        CustomBar: app.globalData.CustomBar, //NavBar整体高度
+        WinHeight: app.globalData.WinHeight, //可使用窗口高度
+        currentTab: 0,
+        // 动态内容
+        playingSongId: -1,
+        // 是否展示底部弹出评论栏
+        showComments: false,
+        // 当前动态的评论
+        curentComments: [],
+        hotDynamicList: []
+    },
 
-  onLoad: function (options) {
-    this.getDynamic();
-  },
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    
-  },
-  /** 自定义顶部栏函数 */
-  //点击上部文字切换页面
-  swichNav: function (e) {
-    var that = this;
-    var current = e.target.dataset.current;
-    if (this.data.currentTab === current) {
-      return false;
-    } else {
-      that.setData({
-        currentTab: current,
-      })
-    }
-  },
-  // 滑动切换页面
-  swiperChange: function (e) {
-    this.setData({
-      currentTab: e.detail.current,
-    })
-  },
-  //跳转到搜索页面
-  navToSearch: function (e) {
-    wx.navigateTo({
-      url: "../../detail/search/search"
-    })
-  },
-  /** 动态中的函数 */
-  // 点击动态，跳转到具体页面
-  bindNavDynamicDetail: function (e) {
-    // dynamicId
-    console.log('bindNavDynamicDetail:' + e.currentTarget.dataset.dynamicId);
-    console.log(e);
-    var dynamicId = e.currentTarget.dataset.dynamicId;
-    wx.navigateTo({
-      url: "../detail/dynamicDetail/dynamicDetail?dynamicId=" + dynamicId
-    })
-  },
-  // 播放/暂停音乐
-  catchPlayAudio: function (e) {
-    // songId
-    console.log('catchPlayAudio:' + e.currentTarget.dataset.songId);
-    console.log(e);
-    var playingSongId = this.data.playingSongId;
-    var curSongId = e.currentTarget.dataset.songId;
-    this.setData({
-      playingSongId: playingSongId == curSongId ? -1 : curSongId
-    })
-  },
-  // 合唱:跳转至歌曲合唱页面
-  catchChorus: function (e) {
-    // songId
-    console.log('catchChorus:' + e.currentTarget.dataset.songId);
-    console.log(e);
-  },
-  // 分享
-  catchShare: function (e) {
-    // songId
-    console.log('catchShare:' + e.currentTarget.dataset.songId);
-    console.log(e);
-  },
-  // 点赞
-  catchUp: function (e) {
-    // dynamicId
-    console.log('catchUp:' + e.currentTarget.dataset.dynamicId);
-    console.log(e);
-  },
-  // 点击评论按钮
-  catchComments: function (e) {
-    // dynamicId
-    // TODO:根据动态ID获取当前评论
-    this.setData({
-      showComments: true,
-      curentComments: [e.currentTarget.dataset.dynamicId]
-    })
-  },
-  // 隐藏底部评论弹出框
-  bindHideComments: function (e) {
-    this.setData({
-      showComments: false
-    })
-  },
+    onLoad: function(options) {
+        this.getDynamic();
+    },
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function() {
 
-  /** 获取动态信息 */
-  getDynamic() {
-    var that = this;
-    var hotDynamicList = [];
-    util.requestFromServer('dynamic', {limit:this.data.hotDynamicList.length + 10}, 'GET').then(res => {
-      if (res.data.data) {
-        console.log('dynamic', res);
-        res.data.data.forEach(v => {
-          var data = {
-            dynamicId: v.id,
-            senderAvatar: app.globalData.userInfo.avatarUrl,
-            senderNickName: app.globalData.userInfo.nickName,
-            creatTime: v.create_time,
-            sendTime: util.getShowTime(v.create_time),
-            content: v.content,
-            images: [],
-            upNum: v.thumbs,
-            commentsNum: v.comments,
-            comments: [],
-            paticipators: v.paticipators,
-            song: {
-              id: v.list_id,
-              cover: `${cloudCallBase}/songs/${v.title}/cover.jpg`,
-              source: `${cloudCallBase}/records/segments/${v.list_id}.mp3`,
-              name: v.title,
-              score: v.scores,
-              needChorus: v.is_private,
-              listenNum: v.listens
-            }
-          }
-          // 添加图片信息
-          for (let i = 0; i < v.pictures; i++) {
-            data.images.push(`${cloudCallBase}/pictures/dynamic/${v.id}/${i}.jpg`);
-          }
-          hotDynamicList.push(data);
+    },
+    /** 自定义顶部栏函数 */
+    //点击上部文字切换页面
+    swichNav: function(e) {
+        var that = this;
+        var current = e.target.dataset.current;
+        if (this.data.currentTab === current) {
+            return false;
+        } else {
+            that.setData({
+                currentTab: current,
+            })
+        }
+    },
+    // 滑动切换页面
+    swiperChange: function(e) {
+        this.setData({
+            currentTab: e.detail.current,
         })
-      }
-    }).then(res => {
-      that.setData({
-        hotDynamicList: hotDynamicList
-      })
-    }).catch(err => {
-      console.log('获取用户动态失败', err)
-    })
-  },
+    },
+    //跳转到搜索页面
+    navToSearch: function(e) {
+        wx.navigateTo({
+            url: "../../detail/search/search"
+        })
+    },
+    /** 动态中的函数 */
+    // 点击动态，跳转到具体页面
+    bindNavDynamicDetail: function(e) {
+        // dynamicId
+        console.log('bindNavDynamicDetail:' + e.currentTarget.dataset.dynamicId);
+        console.log(e);
+        var dynamicId = e.currentTarget.dataset.dynamicId;
+        wx.navigateTo({
+            url: "../../detail/dynamicDetail/dynamicDetail?dynamicId=" + dynamicId
+        })
+    },
+    // 播放/暂停音乐
+    catchPlayAudio: function(e) {
+        // songId
+        console.log('catchPlayAudio:' + e.currentTarget.dataset.songId);
+        console.log(e);
+        var playingSongId = this.data.playingSongId;
+        var curSongId = e.currentTarget.dataset.songId;
+        this.setData({
+            playingSongId: playingSongId == curSongId ? -1 : curSongId
+        })
+    },
+    // 合唱:跳转至歌曲合唱页面
+    catchChorus: function(e) {
+        // songId
+        console.log('catchChorus:' + e.currentTarget.dataset.songId);
+        console.log(e);
+    },
+    // 分享
+    catchShare: function(e) {
+        // songId
+        console.log('catchShare:' + e.currentTarget.dataset.songId);
+        console.log(e);
+    },
+    // 点赞
+    catchUp: function(e) {
+        // dynamicId
+        console.log('catchUp:' + e.currentTarget.dataset.dynamicId);
+        console.log(e);
+    },
+    // 点击评论按钮
+    catchComments: function(e) {
+        // dynamicId
+        // TODO:根据动态ID获取当前评论
+        this.setData({
+            showComments: true,
+            curentComments: [e.currentTarget.dataset.dynamicId]
+        })
+    },
+    // 隐藏底部评论弹出框
+    bindHideComments: function(e) {
+        this.setData({
+            showComments: false
+        })
+    },
+
+    /** 获取动态信息 */
+    getDynamic() {
+        var that = this;
+        var hotDynamicList = [];
+        util.requestFromServer('dynamic', { limit: this.data.hotDynamicList.length + 10 }, 'GET').then(res => {
+            if (res.data.data) {
+                console.log('dynamic', res);
+                res.data.data.forEach(v => {
+                    var data = {
+                            dynamicId: v.id,
+                            senderAvatar: app.globalData.userInfo.avatarUrl,
+                            senderNickName: app.globalData.userInfo.nickName,
+                            creatTime: v.create_time,
+                            sendTime: util.getShowTime(v.create_time),
+                            content: v.content,
+                            images: [],
+                            upNum: v.thumbs,
+                            commentsNum: v.comments,
+                            comments: [],
+                            paticipators: v.paticipators,
+                            song: {
+                                id: v.list_id,
+                                cover: `${cloudCallBase}/songs/${v.title}/cover.jpg`,
+                                source: `${cloudCallBase}/records/segments/${v.list_id}.mp3`,
+                                name: v.title,
+                                score: v.scores,
+                                needChorus: v.is_private,
+                                listenNum: v.listens
+                            }
+                        }
+                        // 添加图片信息
+                    for (let i = 0; i < v.pictures; i++) {
+                        data.images.push(`${cloudCallBase}/pictures/dynamic/${v.id}/${i}.jpg`);
+                    }
+                    hotDynamicList.push(data);
+                })
+            }
+        }).then(res => {
+            that.setData({
+                hotDynamicList: hotDynamicList
+            })
+        }).catch(err => {
+            console.log('获取用户动态失败', err)
+        })
+    },
 })
